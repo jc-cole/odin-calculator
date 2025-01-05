9dd9a8a3b403fe4ae69b95a163b0a37bc15c619f
jc-cole <jaredcole@umass.edu>
2025-01-05 13:52:08 -0500

    Add number and operator button functionality
    only 0-9 and +-*/ are implemented at this time

--- calculator.js
+++ calculator.js
@@ -44,17 +44,33 @@ function operate(expression) {
     return operators[sign](a, b);
 }
 
-// console.log(operate("1 + 2"));
-// console.log(operate("1 * 2"));
-// console.log(operate("1 / 2"));
-// console.log(operate("1 - 2"));
-// console.log(operate("1112 - 1111"));
-// console.log(operate("a - 4"));
-// console.log(operate("1 / 0"));
-// console.log(operate("apples + bananas"));
-// console.log(operate("apples & bananas"));
-// console.log(operate("1 & 2"));
-// console.log(operate(""));
-// console.log(operate("1"));
-// console.log(operate("1 + 2 + 3"));
+function handleButtonPress(button) {
+    const inputWindow = document.querySelector("#calc-display");
+
+    switch (button.className) {
+        case "number":
+            inputWindow.value += button.id;
+            break;
+        case "operator":
+            const parsed = inputWindow.value.split(" ", 3);
+            if (parsed.length === 1) {
+                // the spaces around the operator are required
+                inputWindow.value += ` ${button.id} `;
+            } else { // the only other case is that it has length 3
+                parsed[1] = ` ${button.id} `;
+                inputWindow.value = parsed.join("");
+            }
+            break;
+        default:
+            console.warn(`Button ${button.id} is not implemented yet.`)
+    }
+}
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        handleButtonPress(event.target);
+    })
+});
 
