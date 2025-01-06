11eb61eb45bebdaa7efbd8ea534c6a50a18f2be9
jc-cole <jaredcole@umass.edu>
2025-01-05 20:16:15 -0500

    refactor handleButtonPress function
    Made a buttonHandlers object to hold all of the button press funcitons,
    handleButtonPress just calls the appropriate one

--- calculator.js
+++ calculator.js
@@ -71,81 +71,102 @@ function operate(expression) {
     return result;
 }
 
+const buttonPressEventHandlers = {
+    number: (button, inputWindow) => {
+        if (inputWindow.value == "NaN" || inputWindow.value == "0") {
+            inputWindow.value = button.id;
+        } else {
+            inputWindow.value += button.id;
+        }
+    },
+
+    operator: (button, inputWindow) => {
+        const parsed = inputWindow.value.split(" ", 3);
+        if (parsed.length === 1) {
+            if (inputWindow.value == "NaN") {
+                inputWindow.value = "0";
+            }
+            // the spaces around the operator are required
+            inputWindow.value += ` ${button.id} `;
+        } else { // the only other case is that it has length 3
+            parsed[1] = ` ${button.id} `;
+            inputWindow.value = parsed.join("");
+        }
+    },
+
+    "=": (inputWindow) => {
+        const parsed = inputWindow.value.split(" ");
+        if (parsed.length === 1 || parsed.length === 2) {
+            return; // do nothing because user hasn't completed expression
+        } else if (parsed.length === 3) {
+            inputWindow.value = operate(inputWindow.value);
+        } else {
+            console.error(`Input window in unexpected state ${inputWindow.value}`);
+        }
+    },
+
+    "AC": (inputWindow) => inputWindow.value = "0",
+
+    ".": (inputWindow) => {
+        const last = inputWindow.value.split(" ").at(-1);
+        if (!last.includes('.')) {
+            if (last === "") {
+                inputWindow.value += '0';
+            }
+            inputWindow.value += '.';
+        }
+    },
+
+    "+/-": (inputWindow) => {
+        const parsed = inputWindow.value.split(" ");
+        if (parsed[parsed.length - 1] === "") {
+            return;
+        } else {
+            parsed[parsed.length - 1] = String(Number(parsed[parsed.length - 1]) * -1);
+        }
+        inputWindow.value = parsed.join(" ");
+    },
+
+    "%": (inputWindow) => {
+        parsed = inputWindow.value.split(" ");
+        if (parsed[parsed.length - 1] === "") {
+            return;
+        } else {
+            parsed[parsed.length - 1] = String(Number(parsed[parsed.length - 1]) * 0.01);
+        }
+        inputWindow.value = parsed.join(" ");
+    },
+
+    "++": (inputWindow) => {
+        parse = inputWindow.value.split(" ");
+        parse[parse.length - 1] = Number(parse[parse.length - 1]) + 1;
+        inputWindow.value = parse.join(" ");
+        return;
+    }
+
+}
+
 function handleButtonPress(button) {
     const inputWindow = document.querySelector("#calc-display");
 
-    let parsed = [];
-    let last = "";
+    const classButtonIdentifiers = [
+        "number",
+        "operator"
+    ]
 
-    switch (button.className) {
-        case "number":
-            if (inputWindow.value == "NaN" || inputWindow.value == "0") {
-                inputWindow.value = button.id;
-            } else {
-                inputWindow.value += button.id;
-            }
-            return;
-        case "operator":
-            parsed = inputWindow.value.split(" ", 3);
-            if (parsed.length === 1) {
-                if (inputWindow.value == "NaN") {
-                    inputWindow.value = "0";
-                }
-                // the spaces around the operator are required
-                inputWindow.value += ` ${button.id} `;
-            } else { // the only other case is that it has length 3
-                parsed[1] = ` ${button.id} `;
-                inputWindow.value = parsed.join("");
-            }
-            return;
-    }
+    const idButtonIdentifiers = [
+        "=",
+        "AC",
+        ".",
+        "+/-",
+        "%",
+        "++",
+    ]
 
-    switch (button.id) {
-        case "=":
-            parsed = inputWindow.value.split(" ");
-            if (parsed.length === 1 || parsed.length === 2) {
-                return; // do nothing because user hasn't completed expression
-            } else if (parsed.length === 3) {
-                inputWindow.value = operate(inputWindow.value);
-            } else {
-                console.error(`Input window in unexpected state ${inputWindow.value}`);
-            }
-            return;
-        case "AC":
-            inputWindow.value = "0";
-            return;
-        case ".":
-            last = inputWindow.value.split(" ").at(-1);
-            if (!last.includes('.')) {
-                if (last === "") {
-                    inputWindow.value += '0';
-                }
-                inputWindow.value += '.';
-            }
-            return;
-        case "+/-":
-            parsed = inputWindow.value.split(" ");
-            if (parsed[parsed.length - 1] === "") {
-                return;
-            } else {
-                parsed[parsed.length - 1] = String(Number(parsed[parsed.length - 1]) * -1);
-            }
-            inputWindow.value = parsed.join(" ");
-            return;
-        case "%":
-            parsed = inputWindow.value.split(" ");
-            if (parsed[parsed.length - 1] === "") {
-                return;
-            } else {
-                parsed[parsed.length - 1] = String(Number(parsed[parsed.length - 1]) * 0.01);
-            }
-            inputWindow.value = parsed.join(" ");
-            return;
-        case "++":
-            parse = inputWindow.value.split(" ");
-            parse[parse.length - 1] = Number(parse[parse.length - 1]) + 1;
-            inputWindow.value = parse.join(" ");
-            return;
+    if (classButtonIdentifiers.includes(button.className)) {
+        buttonPressEventHandlers[button.className](button, inputWindow);
+    } else if (idButtonIdentifiers.includes(button.id)) {
+        buttonPressEventHandlers[button.id](inputWindow);
     }
 }
 
